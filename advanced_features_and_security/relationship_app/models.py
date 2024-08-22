abb584db8e640b4a8a9201ac26add3caf901c507
Mhlokza <mhlonipheni9@gmail.com>
2024-08-22 22:29:28 +0200

    advance feature

---
+++ advanced_features_and_security/relationship_app/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, name= 'writer')
+    def __str__(self):
+        return self.name
+    
+class Library(models.Model):
+    name = models.CharField(max_length=20)
+    books = models.ManyToManyField(Book, related_name ='library')
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=20)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = ('admin', 'Admin'), ('librarian', 'Librarian'), ('member', 'Member')
+    role = models.CharField(max_length=20, choices = ROLE_CHOICES)
+
+    def _str_(self):
+        return f"{self.user.username} - {self.role}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = ('admin', 'Admin'), ('librarian', 'Librarian'), ('member', 'Member')
+    role = models.CharField(max_length=20, choices = ROLE_CHOICES)
+
+    def _str_(self):
+        return f"{self.user.username} - {self.role}"
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
