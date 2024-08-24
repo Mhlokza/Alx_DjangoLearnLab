1dcfd8c46d117449101709d493fde93a7a63443f
Mhlokza <mhlonipheni9@gmail.com>
2024-08-24 20:43:15 +0200

    permission

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     def __str__(self):
         return self.name
     class Meta: 
-    permissions = [('can_add_book, Can add Book'), ('can_change_book, Can change book'), ('can_delete_book, Can delete book')]
+        permissions = [('can_add_book, Can add Book'), ('can_change_book, Can change book'), ('can_delete_book, Can delete book')]
 
 
     
