7ce5e5367b5c15ca45c3f6a0d1ad9d82dd432c6a
Mhlokza <mhlonipheni9@gmail.com>
2024-08-25 21:25:26 +0200

    permission

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User,
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -44,4 +44,3 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     instance.userprofile.save()
-
