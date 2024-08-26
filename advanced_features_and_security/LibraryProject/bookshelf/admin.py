7c93e9b5d0c5fcf85be9c26f7b0f02e1b6e880b4
Mhlokza <mhlonipheni9@gmail.com>
2024-08-26 15:09:16 +0200

    useradmin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin as CustomUser
+from .models import User
+class UserAdmin(CustomUserAdmin):
+    pass
+
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
 admin.site.register(Book)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.
 
 # Create your models here.
 class Book(models.Model):
@@ -10,3 +11,20 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField()
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password):
+        if not email:
+            raise valuesError("user should have an email")
+        user = self.model(email= self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user 
+
+    
+    def create_superuser(self,email,password):
+        user = self.create_user(email,password)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using =self._db)
+        user.save()
