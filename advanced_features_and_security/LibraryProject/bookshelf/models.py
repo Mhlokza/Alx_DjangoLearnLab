72321a21d2cc57cb6e76c29b2cbc88de9740c419
Mhlokza <mhlonipheni9@gmail.com>
2024-08-31 20:58:51 +0200

    models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import Permission
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200)
+    publication_year = models.IntegerField()
+
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('The Email field must be set')
+        user = self.model(email = self.normalize_email(email))
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
+        return user
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=False)
+    profile_photo = models.ImageField()
+
+class Permissions(models.Model):
+    premissions = [('can_view', 'Can view'), ('can_create', 'Can view'),
+                   ('can _edit', 'Can edit'), ('can_delete', 'Can delete')]
 
 # Create your models here.
 class Book(models.Model):
