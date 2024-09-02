7ba80bfc264a36fbb9ba53df75279e48390dc6f8
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-09-02 15:09:19 +0000

    api project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics.ListAPIView
 from .models import Book
 from .serializers import MyModelSerializer 
+from .serializers import BookSerializer
 
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
