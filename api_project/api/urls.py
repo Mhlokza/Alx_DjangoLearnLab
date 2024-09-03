26b5b3c956e33f28e90cdb90f8d5fcc1d6fab947
Mhlokza <mhlonipheni9@gmail.com>
2024-09-03 16:11:59 +0200

    api urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,3 +2,19 @@ pythonCopy codefrom django.urls import path
 from .views import BookListCreatAPIView
 
 urlspattens = [path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MyModelViewSet
+
+
+router = DefaultRouter()
+router.register(r'my-models', MyModelViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(" ",api.urls),
+    path((DefaultRouter()), router.urls, include),
+    path("DefaultRouter(),router.urls, include", api.BookViewSet.),
+    path('api/', include(router.urls)),
+]
