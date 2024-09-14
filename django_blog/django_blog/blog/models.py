5e247536608cbed43b8a5df7a29dff625e2f567c
Mhlokza <mhlonipheni9@gmail.com>
2024-09-14 23:46:44 +0200

    blog app

--- django_blog/django_blog/blog/models.py
+++ django_blog/django_blog/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+frome datetime import datetime
+from advanced-api-project.api.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title= models.CharField(max_length=200)
+    content= models.TextField()
+    published_date= models.DateTimeField(auto_now_add=True)
+    author= models.ForeignKey(User, on_delete=CASCATE, name = 'product')
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+
