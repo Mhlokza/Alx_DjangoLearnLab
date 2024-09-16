b93ec296434c54f1f2f671e2b1af5fe389777ad7
Mhlokza <mhlonipheni9@gmail.com>
2024-09-16 23:34:23 +0200

    crud oparation

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('logout/'),
     path('register/', views.RegisterView.as_view(), name= 'login'),
     path('profile/'),
+    path('post/',<int:pk>/delete)
+    path('post/',<int:pk>/update)
+    path('post/', new)
 ]
